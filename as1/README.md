99a788ba7156d0f785645f12b4519ce9278315d8
Matt Osorio <mattosorio@Matts-MacBook-Air.local>
2024-02-14 15:41:21 -0800

    as1- final

---
+++ as1/README.md
@@ -0,0 +1,17 @@
+## Assingmnet 1
+Matthew Osorio
+2/14/23
+
+## How to run program
+Copy and make these commands;
+
+'''
+mkdir build
+cd build
+cmake ..
+make
+./as1
+'''
+
+## Extra Credit
+For extra credit I added a John Rambo mesh to the window. I added "john_rambo.glb" to the meshes file, I configured the file using the cmake method "configure_file()" to be able to load the mesh. I loaded it and you should see the John Rambo model.
--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -1,5 +1,18 @@
 #include "raylib-cpp.hpp"
 
+template<typename T>
+concept Transformer = requires(T t, raylib::Transform m){
+    { t.operator()(m) } -> std::convertible_to<raylib::Transform>;
+};
+
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Transform backupTransform= model.transform;
+    model.transform = transformer(backupTransform);
+    model.Draw({});
+    model.GetTransformedBoundingBox().Draw();
+    model.transform = backupTransform;
+}
+
 int main(){
     raylib::Window window(700, 550, "CS381 - Assingment 1");
     raylib::Camera camera(
@@ -9,17 +22,40 @@ int main(){
         45,
         CAMERA_PERSPECTIVE
     );
-    raylib::Mesh shipMesh();
-    raylib::Model ship = LoadModelFromMesh(raylib::Mesh ship());
+
+    raylib::Model plane("meshes/PolyPlane.glb");
+    plane.transform= raylib::Transform(plane.transform).Scale(3,3,3);
+
+    raylib::Model ship("meshes/SmitHouston_Tug.glb");
+    ship.transform= raylib::Transform(ship.transform).RotateXYZ(raylib::Degree(90),0, raylib::Degree(90));
+
+    raylib::Model rambo("meshes/john_rambo.glb");
+    rambo.transform= raylib::Transform(rambo.transform).Scale(150, 50, 150).RotateX(raylib::Degree(180)).RotateY(raylib::Degree(180));
+
+    SetTargetFPS(60);
+
     while(!window.ShouldClose()){
+        int rotation=0;
         window.BeginDrawing();
             camera.BeginMode();
-                ship.Draw(
-                    raylib::Vector3(0,0,0),
-                    100,
-                    BLUE
-                );
-
+                DrawBoundedModel(plane, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({0,0,0});
+                });
+                DrawBoundedModel(plane, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({-100,100,0}).Scale(1,-1,1).RotateZ(raylib::Degree(180));
+                });
+                DrawBoundedModel(ship, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({-200,0,0});
+                });
+                DrawBoundedModel(ship, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({200,0,0}).RotateZ(raylib::Degree(180));
+                });
+                DrawBoundedModel(ship, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({100,100,0}).Scale(1,2,1).RotateZ(raylib::Degree(270));
+                });
+                DrawBoundedModel(rambo, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({-75,0,-200}).RotateX(raylib::Degree(25));
+                });
             camera.EndMode();
         window.EndDrawing();
     }
