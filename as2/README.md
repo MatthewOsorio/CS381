d8e5496f0ea18294980195294573a6c455c9378f
Matt Osorio <mattosorio@Matts-Air.attlocal.net>
2024-02-18 11:55:54 -0800

    as2

---
+++ as2/README.md
@@ -0,0 +1,24 @@
+## Assingmnet 2
+Matthew Osorio
+2/18/23
+
+## How to run program
+Copy and make these commands;
+
+'''
+mkdir build
+cd build
+cmake ..
+make
+./as2
+'''
+
+## Questions
+5) No I don't think we can simplify the problem by having the camera be remapped to specific world axes because raylib uses a differnt coordinate system. Raylib has a left handing coordinate system whereas I believe in the real world we have a right handed coordinate system.
+
+## Extra Credit
+For extra credit I added three among us meshes to the scence. I thought it would be fun to have the plan to have something to fly around.
+
+I believe it would be quite hard to implement camera movement. Assuming we want a first person POV of the plan, we need to continuous change the camera position based on the positon and angle of the plane. Since the plane is always moving that can make things complicated.
+
+To make the plane move naturally, I believe we can modify the heading variable in accordance to the position. For example if we flying in the right direction and then we decide to fly the plan towards the camera, we would have to change heading so the plane gradually turns unit we are no longer turning.
---
+++ as2/src/as1.cpp
@@ -0,0 +1,103 @@
+#include "raylib-cpp.hpp"
+#include "skybox.hpp"
+template<typename T>
+concept Transformer = requires(T t, raylib::Transform m){
+    { t.operator()(m) } -> std::convertible_to<raylib::Transform>;
+};
+
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Transform backupTransform= model.transform;
+    model.transform = transformer(backupTransform);
+    model.Draw({});
+    model.GetTransformedBoundingBox().Draw();
+    model.transform = backupTransform;
+}
+
+int main(){
+    raylib::Window window(700, 550, "CS381 - Assingment 1");
+    raylib::Camera camera(
+        raylib::Vector3(0, 120, -750),
+        raylib::Vector3(0, 0, 0),
+        raylib::Vector3::Up(),
+        45,
+        CAMERA_PERSPECTIVE
+    );
+
+    cs381::SkyBox skybox("textures/skybox.png");
+
+    auto mesh = raylib::Mesh::Plane(10'000, 10'000, 50, 50,25);
+    raylib::Model ground = ((raylib::Mesh*)&mesh) -> LoadModelFrom();
+    raylib::Texture grass("textures/grass.jpg");
+    grass.SetFilter(TEXTURE_FILTER_BILINEAR);
+    grass.SetWrap(TEXTURE_WRAP_REPEAT);
+    ground.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = grass;
+
+    
+    raylib::Model amongus("meshes/among_us_character.glb");
+    amongus.transform= raylib::Transform(amongus.transform).Scale(1,1,1);
+    
+    raylib::Model plane("meshes/PolyPlane.glb");
+
+    float speed= 5;
+    raylib::Vector3 position = {0, 0, 0};
+    raylib::Degree heading = 60;
+
+    while(!window.ShouldClose()){
+        window.ClearBackground(raylib::Color::Black());
+        window.BeginDrawing();
+            camera.BeginMode();
+                skybox.Draw();
+                ground.Draw({0,0,0});
+
+                DrawBoundedModel(plane, [&position, &heading](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate(position).RotateY(heading);
+                });
+
+                DrawBoundedModel(amongus, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({0,0,-50});
+                });
+
+                DrawBoundedModel(amongus, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({100 ,0, 70});
+                });
+
+                DrawBoundedModel(amongus, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({-100 ,0, 70});
+                });
+
+                raylib::Vector3 velocity = {speed* cos(heading.RadianValue()), 0 , -speed *sin(heading.RadianValue())};
+                position += velocity * window.GetFrameTime();
+            
+                if(IsKeyDown(KEY_W)){
+                    position.z +=1;
+                    heading += 1;
+                }
+                if(IsKeyDown(KEY_S)){
+                    position.z -=1;
+                    heading += 1;
+                }
+                //turn left
+                if(IsKeyDown(KEY_A)){
+                    position.x +=1;
+                    heading += 1;
+                }
+                //turn right
+                if(IsKeyDown(KEY_D)){
+                    position.x -=1;
+                    heading += 1;
+                }
+                if(IsKeyDown(KEY_Q)){
+                    position.y +=1;
+                    heading += 1;
+                }
+                if(IsKeyDown(KEY_E)){
+                    position.y -=1;
+                    heading += 1;
+                }
+
+
+            camera.EndMode();
+        window.EndDrawing();
+    }
+    return 0;
+}
