e564d3dc92e93651d3c1ef54fb30c3875e5972c3
Matt Osorio <mattosorio@Matts-MacBook-Air.local>
2024-05-04 23:28:21 -0700

    as9 final

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -9,7 +9,7 @@ void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
     raylib::Transform backupTransform= model.transform;
     model.transform = transformer(backupTransform);
     model.Draw({});
-    model.GetTransformedBoundingBox().Draw();
+    //model.GetTransformedBoundingBox().Draw();
     model.transform = backupTransform;
 }
 
@@ -23,14 +23,11 @@ int main(){
         CAMERA_PERSPECTIVE
     );
 
-    raylib::Model plane("meshes/PolyPlane.glb");
-    plane.transform= raylib::Transform(plane.transform).Scale(3,3,3);
-
-    raylib::Model ship("meshes/SmitHouston_Tug.glb");
-    ship.transform= raylib::Transform(ship.transform).RotateXYZ(raylib::Degree(90),0, raylib::Degree(90));
+    raylib::Model little_girl("meshes/little_girl.glb");
+    little_girl.transform= raylib::Transform(little_girl.transform).Scale(90.0, 90.0, 90.0).RotateY(raylib::Degree(180));
 
     raylib::Model rambo("meshes/john_rambo.glb");
-    rambo.transform= raylib::Transform(rambo.transform).Scale(150, 50, 150).RotateX(raylib::Degree(180)).RotateY(raylib::Degree(180));
+    rambo.transform= raylib::Transform(rambo.transform).Scale(150, 150, 150).RotateX(raylib::Degree(180)).RotateY(raylib::Degree(-180));
 
     SetTargetFPS(60);
 
@@ -38,23 +35,13 @@ int main(){
         int rotation=0;
         window.BeginDrawing();
             camera.BeginMode();
-                DrawBoundedModel(plane, [](raylib::Transform t) -> raylib::Transform{
+                /*
+                DrawBoundedModel(little_girl, [](raylib::Transform t) -> raylib::Transform{
                     return t.Translate({0,0,0});
                 });
-                DrawBoundedModel(plane, [](raylib::Transform t) -> raylib::Transform{
-                    return t.Translate({-100,100,0}).Scale(1,-1,1).RotateZ(raylib::Degree(180));
-                });
-                DrawBoundedModel(ship, [](raylib::Transform t) -> raylib::Transform{
-                    return t.Translate({-200,0,0});
-                });
-                DrawBoundedModel(ship, [](raylib::Transform t) -> raylib::Transform{
-                    return t.Translate({200,0,0}).RotateZ(raylib::Degree(180));
-                });
-                DrawBoundedModel(ship, [](raylib::Transform t) -> raylib::Transform{
-                    return t.Translate({100,100,0}).Scale(1,2,1).RotateZ(raylib::Degree(270));
-                });
-                DrawBoundedModel(rambo, [](raylib::Transform t) -> raylib::Transform{
-                    return t.Translate({-75,0,-200}).RotateX(raylib::Degree(25));
+                */
+                DrawBoundedModel(little_girl, [](raylib::Transform t) -> raylib::Transform{
+                    return t.Translate({100,0,-100});
                 });
             camera.EndMode();
         window.EndDrawing();
